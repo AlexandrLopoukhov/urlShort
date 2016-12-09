be72109457daeffa11d209c5ab1da7886abcfa9b
Alexandr Lopoukhov <alexandrlopoukhov@gmail.com>
2016-12-09 12:26:07 +0300

    generate short url

--- src/main/java/urlShort/utils/UrlGenarator.java
+++ src/main/java/urlShort/utils/UrlGenarator.java
@@ -1,10 +1,25 @@
 package urlShort.utils;
 
+import java.security.SecureRandom;
+
 /**
  * Created by admin on 12/7/2016.
  */
 public class UrlGenarator {
+    private static final int SHORT_URL_LENGTH = 5;
+    private static SecureRandom random = new SecureRandom();
     public static String generate() {
-        return "2";
+        StringBuilder password = new StringBuilder();
+        while (password.length() < SHORT_URL_LENGTH) {
+            char character = (char) random.nextInt(Character.MAX_VALUE);
+            if ((character >= 'a' && character <= 'z')
+                    || (character >= 'A' && character <= 'Z')
+                    || (character >= '0' && character <= '9')) {
+                password.append(character);
+            }
+        }
+        return password.toString();
+
+        //return "2";
     }
 }
